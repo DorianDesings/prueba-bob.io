9e16c0f605b3031beed701661ec2929910b93e33
Dorian Desings <dorianoriginaldesings@gmail.com>
2020-01-26 19:03:44 +0100

    Estilo del formulario actualizado

--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -24,7 +24,7 @@ const UserForm = () => {
     }
 
     useEffect(() => {
-        getUserInfo()
+        if (id !== undefined) getUserInfo()
     }, [])
 
     const handleBags = (e) => {
@@ -58,31 +58,33 @@ const UserForm = () => {
         const newUser = { name: user.name, bags: user.bags }
         if (id === undefined) {
             axios.post('http://localhost:4000/addUser', newUser)
+            history.goBack()
             return dispatch({
                 type: "ADD_USER",
                 payload: newUser
             })
         } else {
             axios.put(`http://localhost:4000/update/${id}`, newUser)
+            history.goBack()
         }
-
-        history.goBack()
     }
 
     return (
         <form className="form" onSubmit={handleSubmit}>
             <div className="form__field">
                 <label htmlFor="name" className="form__label">Name</label>
-                <input type="text" id="name" className="form__name" name="name" onChange={handleChange} defaultValue={user.name} />
+                <input type="text" id="name" className="form__name" name="name" onChange={handleChange} />
             </div>
             <div className="form__field">
                 <Bags numberOfBags={user.bags} changeBags={handleBags} />
             </div>
-            <div className="form__field">
-                <input type="submit" className="form__submit" disabled={disabled} />
-            </div>
-            <div className="form__field">
-                <input type="button" className="form__cancel" value="Cancel" onClick={handleBack} />
+            <div className="form__buttons">
+                <div className="form__field">
+                    <input type="submit" className="form__button form__button--submit" disabled={disabled} />
+                </div>
+                <div className="form__field">
+                    <input type="button" className="form__button form__button--cancel" value="Cancel" onClick={handleBack} />
+                </div>
             </div>
         </form>
     )
